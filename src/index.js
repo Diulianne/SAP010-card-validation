e3b8fe737f4ace18a6ebcf2f44cb3eeb4c0e8e7e
Diulianne Oliveira <diulicarine@gmail.com>
2023-04-27 10:18:55 -0400

    parte do validador completa e sendo chamado no index, o resultado é mostrado no console

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-console.log(validator);
+console.log();
+//const nomeUsuario = document.getElementsByClassName("nomeDocartão");
+//const dataDiaDoMes = document.getElementsByClassName("dataDia");
+//const dataDoMes = document.getElementsByClassName("dataMes");
 
+window.validarCartao = function () {//window é um objeto que se refere a janela inteira. Peguei o validarCartão e transformei em uma função que vai chamar os seguintes comandos:
+  const numeroCartaoElement = document.getElementById("numeroDoCartao"); //Pega o elemento do html e tranforma em uma constante
 
-const nomeUsuario = document.getElementsByClassName("nomeDocartão");
-const numeroCartao = document.getElementsByClassName("numeroDocartão");
-const dataDiaDoMes = document.getElementsByClassName("dataDia");
-const dataDoMes = document.getElementsByClassName("dataMes");
+  const isValid = validator.isValid(numeroCartaoElement.value); //criei uma constante chamanda isValid que pega do validator a função isValid para que verificar se o valor do numeroCartaoElement é verdadeiro ou falso
+  console.log(isValid); // mostra no meu console se é verdadeiro ou falso
 
-
-
+  const maskedCreditCardNumber = validator.maskify(numeroCartaoElement.value);//Criei uma constante chamada maskedCreditCardNumber que que pega do validator a função maskify e passa o elemento numeroCartaoElement para ser mascarado
+  numeroCartaoElement.value = maskedCreditCardNumber; //aqui é onde o valor do numeroCartaoElement vai ser mascarado
+};
--- src/validator.js
+++ src/validator.js
@@ -1,7 +1,36 @@
 const validator = {
-  isValid: function (numeroCartao) { 
+  // eslint-disable-next-line no-unused-vars
+  isValid(creditCardNumber) {
+    // Accept only digits, dashes or spaces
+    const removeSpace = creditCardNumber.split(" ").join("");
+    let doubleNum = false,
+      sum = 0;
 
-  }
+    if (removeSpace.length < 1) return false;
+
+    removeSpace
+      .split("")
+      .reverse()
+      .forEach((reverseNum) => {
+        reverseNum = parseInt(reverseNum);
+        if (doubleNum) {
+          reverseNum * 2 > 9
+            ? (sum += reverseNum * 2 - 9)
+            : (sum += reverseNum * 2);
+        } else {
+          sum += reverseNum;
+        }
+        doubleNum = !doubleNum;
+      });
+
+    return sum % 10 === 0;
+  },
+
+
+  maskify(creditCardNumber) {
+    const last4Digits = creditCardNumber.slice(-4);
+    return last4Digits.padStart(creditCardNumber.length, "#");
+  },
 };
 
 export default validator;
